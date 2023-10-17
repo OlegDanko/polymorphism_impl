8b3753ddbe4cbbe6a64481ae5395ef048d23b25e
Oleg Danko <olegdanko@outlook.com>
2023-10-17 05:42:06 +0300

    Added shapes variant implementation

---
+++ ShapesVariant.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "BasicShapes.hpp"
+
+#include <variant>
+#include <algorithm>
+
+using ShapeVariant_t = std::variant<Circle, AARect>;
+
+float area(const ShapeVariant_t& shape) {
+    switch(shape.index()) {
+    case 0:
+        return area(std::get<0>(shape));
+    case 1:
+        return area(std::get<1>(shape));
+    default:
+        return {};
+    }
+}
+
+ShapeVariant_t make_circle_variant(float x, float y, float r) {
+    return {Circle{x, y, r}};
+}
+ShapeVariant_t make_aa_rect_variant(float x0, float y0, float x1, float y1) {
+    auto [x_0, x_1] = std::minmax(x0, x1);
+    auto [y_0, y_1] = std::minmax(y0, y1);
+    return {AARect{{x0, y0}, {x1, y1}}};
+}
--- main_variant.cpp
+++ main_variant.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include "ShapesMaker.hpp"
-#include "ShapesPolymorphism.hpp"
+#include "ShapesVariant.hpp"
 
 int main() {
-    auto shapes = make_shapes(10000, make_circle, make_aa_square);
+    auto shapes = make_shapes(10000, make_circle_variant, make_aa_rect_variant);
     double total_area_diff = 0.0f;
 
     for (size_t a = 0; a < shapes.size() - 1; ++a) {
         for (size_t b = a + 1; b < shapes.size(); ++b) {
-            total_area_diff += std::abs(shapes.at(a)->area() - shapes.at(b)->area());
+            total_area_diff += std::abs(area(shapes.at(a)) - area(shapes.at(b)));
         }
     }
+
+//    std::cout << total_area_diff << std::endl;
 
     return (int)total_area_diff;
 }
